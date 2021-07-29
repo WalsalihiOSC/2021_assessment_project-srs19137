27f77c4aee89819f1a9a40c73a9a7b07ffcb73e6
srs19137 <79232140+srs19137@users.noreply.github.com>
2021-07-29 14:16:00 +1200

    message

--- CSC3_Assessment.py
+++ CSC3_Assessment.py
@@ -3,20 +3,26 @@ import math
 
 ROOT = tk.Tk()
 
-loginframe = tk.Frame(master=None)
-loginframe.grid(column=0,row=0)
-testlabellogin = tk.Label(master=loginframe,text='Login Screen')
-testlabellogin.grid()
+class LoginScreen:
+    def __init__(self):
+        loginframe = tk.Frame(master=None)
+        loginframe.grid(column=0,row=0)
+        testlabellogin = tk.Label(master=loginframe,text='Login Screen')
+        testlabellogin.grid()
 
-levelselect = tk.Frame(master=None)
-levelselect.grid(column=0,row=0)
-testlabellevelselect = tk.Label(master=levelselect,text='Level Select')
-testlabellevelselect.grid()
+class LevelSelect:
+    def __init__(self):
+        levelselect = tk.Frame(master=None)
+        levelselect.grid(column=0,row=0)
+        testlabellevelselect = tk.Label(master=levelselect,text='Level Select')
+        testlabellevelselect.grid()
 
-questions = tk.Frame(master=None)
-questions.grid(column=0,row=0)
-testlabelquestions = tk.Label(master=questions,text='Questions')
-testlabelquestions.grid()
+class Questions:
+    def __init__(self):
+        questions = tk.Frame(master=None)
+        questions.grid(column=0,row=0)
+        testlabelquestions = tk.Label(master=questions,text='Questions')
+        testlabelquestions.grid()
 
 tk.Button(text='Raise Login Frame',command=levelselect.tkraise).grid()
 tk.Button(text='Raise Level Select Frame',command=loginframe.tkraise).grid()
