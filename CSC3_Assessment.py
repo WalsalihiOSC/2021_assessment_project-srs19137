ca399c74c672b6f60fb704391782b731f0cbc0b9
srs19137 <79232140+srs19137@users.noreply.github.com>
2021-07-29 14:15:42 +1200

    (currently untested) Added Questions frame and seperating into classes

--- CSC3_Assessment.py
+++ CSC3_Assessment.py
@@ -3,17 +3,22 @@ import math
 
 ROOT = tk.Tk()
 
-loginframe = tk.Frame(master=None,width=200,height=200)
+loginframe = tk.Frame(master=None)
 loginframe.grid(column=0,row=0)
 testlabellogin = tk.Label(master=loginframe,text='Login Screen')
 testlabellogin.grid()
 
-levelselect = tk.Frame(master=None,width=200,height=200)
+levelselect = tk.Frame(master=None)
 levelselect.grid(column=0,row=0)
-
 testlabellevelselect = tk.Label(master=levelselect,text='Level Select')
 testlabellevelselect.grid()
 
-tk.Button(text='Login to Level Select',command=levelselect.tkraise).grid()
-tk.Button(text='Level Select to Login',command=loginframe.tkraise).grid()
+questions = tk.Frame(master=None)
+questions.grid(column=0,row=0)
+testlabelquestions = tk.Label(master=questions,text='Questions')
+testlabelquestions.grid()
+
+tk.Button(text='Raise Login Frame',command=levelselect.tkraise).grid()
+tk.Button(text='Raise Level Select Frame',command=loginframe.tkraise).grid()
+tk.Button(text='Raise Questions Frame',command=questions.tkraise).grid()
 ROOT.mainloop()
